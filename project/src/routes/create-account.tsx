e00f71c3ac12e7651d8f1fdd45ddc4f841db43b9
hyerim <hyerimiya1216@gmail.com>
2024-07-11 23:52:30 +0900

    Feat : style-components 분리

--- project/src/routes/create-account.tsx
+++ project/src/routes/create-account.tsx
@@ -2,57 +2,16 @@ import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { FirebaseError } from 'firebase/app';
 
-import { styled } from "styled-components";
 import { auth } from '../firebase';
 import { useNavigate, Link } from 'react-router-dom';
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  margin-top: 10px;
-  font-weight: 600;
-  color: tomato;
-`;
-
-export const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
+import {
+  Form,
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from '../components/auth-components';
 
 export default function CreateAccount() {
   const navigate = useNavigate();
